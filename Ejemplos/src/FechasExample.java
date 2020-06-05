840738c46600eb90c7ea1ff749658644861f8a4f
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-06-05 21:04:36 +0200

    examples

--- Ejemplos/src/FechasExample.java
+++ Ejemplos/src/FechasExample.java
@@ -1,6 +1,15 @@
+import java.io.ObjectInputStream.GetField;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class FechasExample {
 
@@ -44,6 +53,61 @@ public class FechasExample {
 		calendar.setTime(new Date(calendar2.getTimeInMillis()-calendar1.getTimeInMillis()));
 		
 		calendar.set(Calendar.DAY_OF_YEAR, +90);
+		
+		//Ejercicios
+		
+		//1-Lista de 10 personas ordenada por fecha de nacimientos (DATE)
+		Cliente cliente1= new Cliente("Pedro", new Date(1995, 01, 01));
+		Cliente cliente2= new Cliente("Luisa", new Date(1993, 04, 01));
+		Cliente cliente3= new Cliente("Miguel", new Date(1998, 01, 01));
+		Cliente cliente4= new Cliente("Andres", new Date(1995, 06, 01));
+		Cliente cliente5= new Cliente("Maria", new Date(1982, 06, 01));
+		Cliente cliente6= new Cliente("Lucas", new Date(1999, 04, 01));
+		Cliente cliente7= new Cliente("Ana", new Date(1991, 01, 01));
+		Cliente cliente8= new Cliente("Marta", new Date(1992, 01, 01));
+		Cliente cliente9= new Cliente("Jose", new Date(1999, 04, 02));
+		
+		List<Cliente> clientes= new ArrayList<Cliente>();
+		clientes.add(cliente1);
+		clientes.add(cliente2);
+		clientes.add(cliente3);
+		clientes.add(cliente4);
+		clientes.add(cliente5);
+		clientes.add(cliente6);
+		clientes.add(cliente7);		
+		clientes.add(cliente8);
+		clientes.add(cliente9);	
+	
+//		Collections.sort(clientes);
+//		clientes.stream().forEach(System.out::println);
+		
+		Collections.sort(clientes, new Comparator<Cliente>() {
+			  public int compare(Cliente o1, Cliente o2) {			    
+			      return o1.getBirthDate().compareTo(o2.getBirthDate());
+			  }
+			});
+	
+				
+		clientes.stream().forEach(System.out::println);
+		
+		//2-Metodo que calcule la diferencia entre el nacimiento de dos personas
+		
+		System.out.println(getDiasDiferencia(cliente1, cliente2));
+		
+		//3-Utilizando calendar decirle a una personas si su cumpleaños el año que viene cae en fin de semana
+
+		Calendar cal= new GregorianCalendar(2020,cliente1.getBirthDate().getMonth(),cliente1.getBirthDate().getDay());
+			
+		if(cal.get(Calendar.DAY_OF_WEEK)==5||cal.get(Calendar.DAY_OF_WEEK)==6) {
+			System.out.println(" tu proximo cumpleaños cae en finde" + cal.get(Calendar.DAY_OF_WEEK) +","+cal.get(Calendar.MONTH));
+		}	
+		
+		
+	}
+	
+	static long getDiasDiferencia(Cliente cliente1, Cliente cliente2) {
+				
+	return TimeUnit.DAYS.convert(cliente1.getBirthDate().getTime()-cliente2.getBirthDate().getTime(), TimeUnit.MILLISECONDS);
 		
 	}
 
