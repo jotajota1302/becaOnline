925582537d80b2728ff9b7cd5e44d2c382045756
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-06-08 21:00:51 +0200

    commit

--- Ejemplos/src/PersonalInfo.java
+++ Ejemplos/src/PersonalInfo.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class PersonalInfo {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		
 		
 		Map<String,Contact> personas= new HashMap<String,Contact>();
@@ -21,6 +21,12 @@ public class PersonalInfo {
 		personas.put(p3.getNif(), p3);
 		
 		
+		start(personas);
+		
+	}
+
+	public static void start(Map<String, Contact> personas) {
+	
 		System.out.println("Introduce NIF : ");
 		
 		Scanner scanner= new Scanner(System.in);
@@ -29,22 +35,32 @@ public class PersonalInfo {
 		Contact p= personas.get(nif);
 		
 		SimpleDateFormat formatter= new SimpleDateFormat("dd/MM/yyyy");
-		Date date=formatter.parse(p.getBirthDate());
+		Date date = null;
+		try {
+			date = formatter.parse(p.getBirthDate());	
+			
+		} catch (ParseException e) {			
+			System.out.println("Formato de fecha incorrecto: ");
+			System.out.println("Introduce fecha correcta: " );
+			scanner= new Scanner(System.in);
+			String fecha=scanner.next();
+			p.setBirthDate(fecha);
+			start(personas);
+		}			
 		
 		SimpleDateFormat printer= new SimpleDateFormat("'El' EEEEEEEEEE ,dd 'de' MMMMMMMM 'de' yyyy");
-		System.out.println("La fecha de nacimiento es: " + printer.format(date));
-		
-		//2-controlar las excepciones utilizando los bloques try catch, haciendola robusta antes errores de entrada de datos
-		
-	
-		
+		try {
+			System.out.println("La fecha de nacimiento es: " + printer.format(date));
+		} catch (Exception e) {
+			System.out.println("Revisa el patron:");
+			throw e;
+		}
 		//3-Enum -> Lista de errores posibles fuera un ENUM y en el catch imprimamos el tipo de error que tenemos
 		
 		//MalFormatoDeFecha
 		//NoVieneLaFecha
 		//UsuarioNoExistente
 		//NifIncorrecto(minimo 8 digitos)
-		
 	}
 	
 	
