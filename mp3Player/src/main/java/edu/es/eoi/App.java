d0597c3e455e520abc57c4c9404c3257ad46328b
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-06-09 19:42:49 +0200

    first commit

---
+++ mp3Player/src/main/java/edu/es/eoi/App.java
@@ -0,0 +1,20 @@
+package edu.es.eoi;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+public class App {
+	
+	public static void main(String[] args) throws FileNotFoundException, JavaLayerException {
+			
+		Player player= new Player(new FileInputStream(new File("C:/workspaces/becaOnline/mp3Player/src/main/resources/file.mp3")));
+		
+		player.play();
+		
+	}
+
+}
