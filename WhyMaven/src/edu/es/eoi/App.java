f9f5b0f1605dc87398aa3c19f6fd5175cdf61133
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-06-09 17:35:03 +0200

    commit mp3 player

---
+++ WhyMaven/src/edu/es/eoi/App.java
@@ -0,0 +1,22 @@
+package edu.es.eoi;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+public class App {
+	
+	public static void main(String[] args) throws FileNotFoundException, JavaLayerException {
+				
+		System.out.println("Welcome " + args[0]);
+		
+		Player player= new Player(new FileInputStream(new File("C:/workspaces/becaOnline/WhyMaven/src/ficheros/file.mp3")));
+		
+		player.play();
+		
+	}
+
+}
