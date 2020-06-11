ccf36768a52af636df634147c41da9d7bef6f79a
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-06-11 19:01:09 +0200

    commit

--- TiendaOnlineMaven/src/test/java/TiendaOnlineTest.java
+++ TiendaOnlineMaven/src/test/java/TiendaOnlineTest.java
@@ -1,10 +1,31 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import org.junit.jupiter.api.Test;
+
+import com.google.gson.Gson;
 
 import edu.es.eoi.entity.Producto;
 import edu.es.eoi.repository.ProductoRepository;
+import edu.es.eoi.util.TiendaOnlineUtils;
 
 class TiendaOnlineTest {
 
+	
+	@Test
+	void leerJson() throws FileNotFoundException {
+
+		TiendaOnlineUtils util= new TiendaOnlineUtils();	
+		Gson gson = new Gson();	
+
+		FileReader reader=new FileReader(util.getFileFromResources("productos.json"));
+			
+		Producto producto = gson.fromJson(reader, Producto.class);
+	
+		System.out.println(producto);
+		
+	}
+	
 	
 	@Test
 	void testGuardar() {
