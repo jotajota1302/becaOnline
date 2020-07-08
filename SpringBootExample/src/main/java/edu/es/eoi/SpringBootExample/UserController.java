d5c07fbee04f0558efcd5bfa72ef6c561afb9c74
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-07-08 20:43:23 +0200

    commit

--- SpringBootExample/src/main/java/edu/es/eoi/SpringBootExample/UserController.java
+++ SpringBootExample/src/main/java/edu/es/eoi/SpringBootExample/UserController.java
@@ -1,6 +1,5 @@
 package edu.es.eoi.SpringBootExample;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,39 +7,39 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping
 public class UserController {
 	
 	@GetMapping("users")	
-	public @ResponseBody String getUsers() throws Exception {
+	public String getUsers() throws Exception {
 		return "return all users ";
 	}
 	
 	@GetMapping("users/{id}/products")	
-	public @ResponseBody String getProductsFromUser(@PathVariable String id) throws Exception {
+	public String getProductsFromUser(@PathVariable String id) throws Exception {
 		return "return all products from user:" +id;
 	}
 	
 	@GetMapping("users/{id}")	
-	public @ResponseBody String getUser(@PathVariable String id) throws Exception {
+	public String getUser(@PathVariable String id) throws Exception {
 		return "return only with id: " + id;
 	}
 	
 	@PostMapping("users")
-	public @ResponseBody String createUser(@RequestParam String name) {
+	public String createUser(@RequestParam String name) {
 		return "He creado un nuevo usuario : " + name;
 	}
 	
 	@PutMapping("users/{id}")
-	public @ResponseBody String updateUser(@RequestParam String name,@PathVariable String id) {
+	public String updateUser(@RequestParam String name,@PathVariable String id) {
 		return "He updateado el usuario: " + name +", con id:" + id;
 	}
 	
 	@DeleteMapping("users/{id}")
-	public @ResponseBody String deleteUser(@PathVariable String id) {
+	public String deleteUser(@PathVariable String id) {
 		return "He borrado el usuario con id:" + id;
 	}
 	
