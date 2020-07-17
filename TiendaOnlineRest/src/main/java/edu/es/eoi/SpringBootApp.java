79a4419d7c7c6ce3204012656c01c6fa1e0122b3
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-07-17 17:59:44 +0200

    commit

--- TiendaOnlineRest/src/main/java/edu/es/eoi/SpringBootApp.java
+++ TiendaOnlineRest/src/main/java/edu/es/eoi/SpringBootApp.java
@@ -4,11 +4,13 @@ package edu.es.eoi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EntityScan(basePackages = "edu.es.eoi")
+@EnableDiscoveryClient
 public class SpringBootApp {
 
 	public static void main(String[] args) {
