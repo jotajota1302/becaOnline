96b7211ebb4d40057976cc04897eb4524261a9ca
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-07-21 20:48:41 +0200

    commit

--- ShopClient/src/main/java/edu/es/eoi/shop/Shop.java
+++ ShopClient/src/main/java/edu/es/eoi/shop/Shop.java
@@ -1,24 +1,34 @@
 package edu.es.eoi.shop;
 
+import java.net.URI;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import edu.es.eoi.dto.ProductDto;
+import edu.es.eoi.dto.UserDto;
 
 public class Shop {
 
 	public static void main(String[] args) {
 		
+		String urlGateway="http://localhost:8080";
+		String urlProducts=urlGateway.concat("/micro-product/products");
+		String urlUsers=urlGateway.concat("/micro-user/users");		
+		
 		//Listado productos
 		
 		RestTemplate restTemplate= new RestTemplate();
 		
-		ResponseEntity<ProductDto[]> response =  restTemplate.getForEntity(
-				  "http://localhost:8080/micro-product/products",ProductDto[].class);		
+		ResponseEntity<ProductDto[]> response = restTemplate.getForEntity(urlProducts,ProductDto[].class);		
 		
 		ProductDto[] product = response.getBody();
+		
+		System.out.println(product.length);
 
-		System.out.println(product);
+		ResponseEntity<UserDto[]> users = restTemplate.getForEntity(urlUsers,UserDto[].class);		
+		
+		System.out.println(users.getBody().length);
 	}
 
 }
