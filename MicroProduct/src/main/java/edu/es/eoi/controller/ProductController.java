bbabdb28f696362ac1940357c7be13a0ffdd13d7
jjimerod <jjimerod@ALC-1JHK3Z2.usersad.everis.int>
2020-07-21 19:43:27 +0200

    remove dtos

--- MicroProduct/src/main/java/edu/es/eoi/controller/ProductController.java
+++ MicroProduct/src/main/java/edu/es/eoi/controller/ProductController.java
@@ -3,6 +3,7 @@ package edu.es.eoi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,26 +25,28 @@ public class ProductController implements ProductInterfaceController {
 
 	@Override
 	public ResponseEntity<ProductDto> findById(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		return ResponseEntity.ok(service.findById(id).get());
 	}
 
 	@Override
 	public ResponseEntity<ProductDto> create(ProductDto dto) {
-		// TODO Auto-generated method stub
-		return null;
+		service.create(dto);
+		return new ResponseEntity<ProductDto>(HttpStatus.CREATED);
 	}
 
 	@Override
 	public ResponseEntity<ProductDto> update(ProductDto dto, Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		if(dto.getId()!=id) {
+			return new ResponseEntity<ProductDto>(HttpStatus.BAD_REQUEST);
+		}
+		service.create(dto);
+		return new ResponseEntity<ProductDto>(HttpStatus.ACCEPTED);
 	}
 
 	@Override
 	public ResponseEntity<ProductDto> delete(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		service.delete(id);
+		return new ResponseEntity<ProductDto>(HttpStatus.ACCEPTED);
 	}
 	
 }
